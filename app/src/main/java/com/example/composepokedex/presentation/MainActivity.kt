dec65bc6dde840fd195352f05b676d18bd40df6b
Egito Filho <egitofilho4@gmail.com>
2023-12-28 23:27:29 -0300

    create a base project, repository and network

---
+++ app/src/main/java/com/example/composepokedex/presentation/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.composepokedex.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.composepokedex.presentation.ui.theme.JetpackComposePokedexTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposePokedexTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemon_list_screen"
+                ) {
+                    composable("pokemon_list_screen") {
+
+                    }
+                    composable(
+                        "pokemon_detail_screen/{pokemonName}",
+                        arguments = listOf(
+                            navArgument("pokemonName") {
+                                type = NavType.StringType
+                            }
+                        )
+                        ) {
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
